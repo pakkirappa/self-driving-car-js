3ac29ab74450d25f3859e35e9ef0e75f66464e29
Vamsi Konakanchi <vamsi@ymtsindia.in>
2024-01-31 15:43:12 +0530

    added lanes and borders for roads

--- main.js
+++ main.js
@@ -2,15 +2,18 @@ const canvas = document.getElementById("myCanvas");
 canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
-const road = new Road(canvas.width / 2, canvas.width * 0.9,10);
-const car = new Car(100, 100, 30, 50);
+const road = new Road(canvas.width / 2, canvas.width * 0.9);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 
 animate();
 
 function animate() {
   car.update();
   canvas.height = window.innerHeight;
-  road.draw(ctx);
-  car.draw(ctx);
+  ctx.save();
+  ctx.translate(0, -car.y + canvas.height * 0.7);
+  road.draw(ctx); // drawing the road
+  car.draw(ctx); // drawing the car
+  ctx.restore();
   requestAnimationFrame(animate);
 }
